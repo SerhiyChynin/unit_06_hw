1b5fc46c3009aab1a5ec25747892f02f824b9c5a
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-13 16:58:47 +0200

    cycle_comp_hw_done

--- src/Homework1.js
+++ src/Homework1.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 class Homework1 extends React.Component{
-    constructor(props, s2=201) {
-        console.log(props.p1)
+    constructor(props) {
         super();
         this.state = {
             s1: props.p1
         }
-        this.s2 = s2;
+        this.s2 = 201;
     }
+    
+    componentDidMount() {
+        let val_1 = this.state.s1;
+        this.setState({ s1: val_1 + 5 })
+
+        let val_2 = this.s2;
+        this.s2 = val_2 + 5; 
+    }
+    
+        buttonHandler = () => {
+            let val_1 = this.state.s1;
+            
+            this.setState({ s1: val_1 + 50})
+
+            let val_2 = this.s2;
+            this.s2 = val_2 + 50;    
+        }
+    
     render() {
-        console.log('Hello')
         return (
             <>
-                {console.log('Baby!')}
+            <div>
+                    {this.state.s1} 
+                </div>
+            <div>
+                    {this.s2}
+                </div>
+                <button onClick={this.buttonHandler}>Push</button>
             </>
+            
         )
     }
 }
